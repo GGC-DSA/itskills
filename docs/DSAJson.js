6459bbc30b95dc04d6cc933f242902928bed1927
Anel <anelcoralic7@gmail.com>
2023-04-21 12:33:43 -0400

    inserted DSA Data and updated DSA tab on website

---
+++ docs/DSAJson.js
@@ -0,0 +1,456 @@
+var treeJson = [
+  {
+    name: "Data Science and Analytics",
+    value: 14,
+    type: "black",
+    level: "red",
+    children: [
+      {
+        name: "Data Scientist",
+        parent: "Data Science and Analytics",
+        value: 12,
+        type: "grey",
+        level: "pink",
+        children: [
+          {
+            name: "Python",
+            parent: "Data Scientist",
+            value: 10,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Python",
+                value: 9,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "Machine Learning",
+            parent: "Data Scientist",
+            value: 8,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Machine Learning",
+                value: 7,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SQL",
+            parent: "Data Scientist",
+            value: 6,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SQL",
+                value: 5,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "R",
+            parent: "Data Scientist",
+            value: 4,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "R",
+                value: 3,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "Data Mining",
+            parent: "Data Scientist",
+            value: 2,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Data Mining",
+                value: 1,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Data Analyst",
+        parent: "Data Science and Analytics",
+        value: 0,
+        type: "grey",
+        level: "pink",
+        children: [
+          {
+            name: "Python",
+            parent: "Data Analyst",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Python",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "Machine Learning",
+            parent: "Data Analyst",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Machine Learning",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SQL",
+            parent: "Data Analyst",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SQL",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "R",
+            parent: "Data Analyst",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "R",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "Analysis Skills",
+            parent: "Data Analyst",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Analysis Skills",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Data Architect",
+        parent: "Data Science and Analytics",
+        value: 0,
+        type: "grey",
+        level: "pink",
+        children: [
+          {
+            name: "Machine Learning",
+            parent: "Data Architect",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Machine Learning",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "Python",
+            parent: "Data Architect",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "Python",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "AI",
+            parent: "Data Architect",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "AI",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "PyTorch",
+            parent: "Data Architect",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "PyTorch",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "TensorFlow",
+            parent: "Data Architect",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "TensorFlow",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "JOB 4",
+        parent: "Data Science and Analytics",
+        value: 0,
+        type: "grey",
+        level: "pink",
+        children: [
+          {
+            name: "SKILL 1",
+            parent: "JOB 4",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 1",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 2",
+            parent: "JOB 4",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 2",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 3",
+            parent: "JOB 4",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 3",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 4",
+            parent: "JOB 4",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 4",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 5",
+            parent: "JOB 4",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 5",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "JOB 5",
+        parent: "Data Science and Analytics",
+        value: 0,
+        type: "grey",
+        level: "pink",
+        children: [
+          {
+            name: "SKILL 1",
+            parent: "JOB 5",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 1",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 2",
+            parent: "JOB 5",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 2",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 3",
+            parent: "JOB 5",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 3",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 4",
+            parent: "JOB 5",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 4",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+          {
+            name: "SKILL 5",
+            parent: "JOB 5",
+            value: 0,
+            type: "steelblue",
+            level: "green",
+            children: [
+              {
+                name: "COURSE",
+                parent: "SKILL 5",
+                value: 0,
+                type: "darkblue",
+                level: "purple",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+console.log(JSON.parse(treeJson));
